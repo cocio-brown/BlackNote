d47276b8612b50a1e85b0eeb99be6d7405c8e1d0
MoniKaDimitrova <48559291+MoniKaDimitrova@users.noreply.github.com>
2019-12-17 02:05:06 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,44 @@
 window.addEventListener("DOMContentLoaded", init);
 
-const urlParams = new URLSearchParams(window.location.search);
-
-const artists = urlParams.get("artist");
-
-function init(){
-  if(artists){
-        getArtistsData();
-  }
-  else{
-      console.log("wtf");
-  }
+function init() {
+    if (window.location.href.indexOf("artists") > -1) {
+        getArtists();
+        console.log("artists")
+    } else if (window.location.href.indexOf("gallery") > -1) {
+        getGallery();
+        console.log("gallery")
+} else if (window.location.href.indexOf("contact") > -1) {
+    getContact();
+    console.log("contact")
+} else if (window.location.href.indexOf("about") > -1) {
+    getAbout();
+    console.log("about")
+} else {
+    getArtists();
+   
+}
 }
 
-function getArtistsData(){
-    //console.log("getArtistsData")
+
+function getPages(){
+    console.log("getPages")
+    fetch ("http://mycreativedigitalvision.eu/WP/wordpress/wp-json/wp/v2/pages")
+
+    .then(res=>res.json())
+    .then(handlePages)
+}
+
+function handlePages(myData){
+    console.log(myData)
+    myData.forEach(showPages)
+}
+
+function showPages(pages){
+    console.log(pages)
+}
+
+function getArtists(){
+    //console.log("getArtists")
     fetch("http://mycreativedigitalvision.eu/WP/wordpress/wp-json/wp/v2/artist")
 
     .then(res=>res.json())
@@ -41,4 +65,62 @@ function showArtists(artist){
     artist_text.innerHTML = artist.text_artist;
 
     document.querySelector("#artists").appendChild(postCopy)
+}
+
+function getAbout(){
+    //console.log("getAbout")
+    fetch ("http://mycreativedigitalvision.eu/WP/wordpress/wp-json/wp/v2/pages/397")
+     
+      .then(res=>res.json())
+      .then(showAbout)
+      }
+    
+
+function showAbout(about){
+    console.log(about)
+
+    const template = document.querySelector("template").content;
+    const postCopy = template.cloneNode(true);
+    const p = postCopy.querySelector("p");
+    p.innerHTML = about.content.rendered;
+
+    document.querySelector("#about").appendChild(postCopy)
+}
+
+function getGallery(){
+    //console.log("getGallery")
+    fetch ("http://mycreativedigitalvision.eu/WP/wordpress/wp-json/wp/v2/pages/401")
+
+    .then(res=>res.json())
+    .then(showGallery)
+}
+
+function showGallery(gallery){
+    console.log(gallery)
+
+    const template = document.querySelector("template").content;
+    const postCopy = template.cloneNode(true);
+    const p = postCopy.querySelector("p");
+    p.innerHTML = gallery.content.rendered;
+
+    document.querySelector("#gallery").appendChild(postCopy)
+}
+
+function getContact(){
+    //console.log("getContact")
+    fetch("http://mycreativedigitalvision.eu/WP/wordpress/wp-json/wp/v2/pages/399")
+
+    .then(res=>res.json())
+    .then(showContact)
+}
+
+function showContact(contact){
+    console.log(contact)
+
+    const template = document.querySelector("template").content;
+    const postCopy = template.cloneNode(true);
+    const p = postCopy.querySelector("p");
+    p.innerHTML = contact.content.rendered;
+
+    document.querySelector("#contact").appendChild(postCopy)
 }
